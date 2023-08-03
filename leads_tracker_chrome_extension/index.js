834cb21a67db612421f68cb73e50abe1f9c8fa5c
Kiran_nandhu <98279552+Drtech-859@users.noreply.github.com>
2023-08-03 08:04:45 +0530

    Added files via upload

---
+++ leads_tracker_chrome_extension/index.js
@@ -0,0 +1,57 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+const delInputButton = document.getElementById("del-input-btn")
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
+
+delInputButton.addEventListener("click",function(){
+    let index = myLeads.indexOf(inputEl.value)
+    if(index >= 0){
+        myLeads.splice(index,1)
+    }
+    inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+})
